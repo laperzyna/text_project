13ea5b8d9cf5da107e4f931ff447b71bb29a68e6
laperzyna <90733086+laperzyna@users.noreply.github.com>
2024-04-25 11:21:25 -0700

    added more styling

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
-//let easing = 0.05;
+function preload() {
+  customFont = loadFont('./Inspiration-Regular.ttf');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   rectMode(CENTER);
+  textFont(customFont);
   textAlign(CENTER, CENTER);
 }
 
@@ -11,33 +14,45 @@ function draw() {
   background(0);
   translate(width / 2, height / 2);
   frameRate(35); 
-  let totalBoxes = 20;
+  let totalBoxes = 21;
   let boxesToShow = floor(frameCount / 10);
+  let delayFrames = 30;
 
-  for (let i = 0; i < totalBoxes; i++) {
-    // Only drawing to 20
-    if (i < boxesToShow) {
-      // angle to space out the boxes
+  for (let i = 0; i < totalBoxes - 1; i++) {
+    if (i < boxesToShow - 1) {
       let angle = i * 139.5;
-      // spiral effect - :/
-      let r = sqrt(i) * 85;
+      let r = sqrt(i) * 75;
       let x = r * cos(angle);
       let y = r * sin(angle);
-
+      
       push();
       translate(x, y);
       rotate(angle);
       stroke('#AF251E');
       strokeWeight(2);
       fill(0);
-      rect(0, 0, 80, 80);
+      rect(0, 0, 65, 65);
       fill(255);
       stroke('white');
-      textSize(40);
-      text(i + 1, 0, 0);
+      textSize(45);
+      text(i + 1, 0, -10);
       pop();
     }
   }
+
+  // Draw the 21st box in the center
+  if (boxesToShow >= totalBoxes + delayFrames / 10) {
+    let hover = abs(mouseX - width / 2) < 65 && abs(mouseY - height / 2) < 65;
+    push();
+    stroke('#AF251E');
+    strokeWeight(2);
+    fill(hover ? '#FF6347' : '0');
+    // fill(0);
+    rect(0, 0, 130, 130);
+    fill(255);
+    stroke('white');
+    textSize(65);
+    text(21, 0, -10); // 21st box
+    pop();
+  }
 }
-
-
