dd1b5971143ef43b90fdea529b8c2fe08cad1b03
laperzyna <90733086+laperzyna@users.noreply.github.com>
2024-05-09 00:45:33 -0700

    things

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var sentences = [
     ["Como", "una", "utopía", "sin", "brecha'"],
     ["Yo", "quiero", "ver", "la", "mariposa'", "suelta'"]
 ];
+
 var currentSentence = 0;
 var currentWord = 0;
 var textContainer = document.getElementById('text-container');
@@ -42,128 +43,129 @@ function displayNextWord() {
             textContainer.style.opacity = '1';
             currentSentence++;
             currentWord = 0;
-             // start next sentence
+            // start next sentence
             displayNextWord();
         }, 2000);
     } else {
-        // This is the last sentence, start the butterfly animation
+        // Start the animation at the right time as per your existing logic
         if (!document.getElementById('butterfly').style.animation) {
-            document.getElementById('butterfly').style.animation = 'slideDiagonally 6s ease-in-out infinite';
+            document.getElementById('butterfly').style.animation = 'slideDiagonally 12s ease-in-out infinite';
         }
+
     }
 }
 
 // code snippet for curser repel functionality
 var canvas = document.querySelector("#scene"),
-  ctx = canvas.getContext("2d"),
-  particles = [],
-  amount = 0,
-  mouse = {x:0,y:0},
-  radius = 1;
+    ctx = canvas.getContext("2d"),
+    particles = [],
+    amount = 0,
+    mouse = { x: 0, y: 0 },
+    radius = 1;
 
-var colors = ["#AF251E","#F47888", "#FEFDFE","#F71105", "#787170"];
+var colors = ["#AF251E", "#F47888", "#FEFDFE", "#F71105", "#787170"];
 
 var ww = canvas.width = window.innerWidth;
 var wh = canvas.height = window.innerHeight;
 
-function Particle(x,y){
-  this.x =  Math.random()*ww;
-  this.y =  Math.random()*wh;
-  this.dest = {
-    x : x,
-    y: y
-  };
-  this.r =  Math.random()*5 + 2;
-  this.vx = (Math.random()-0.5)*20;
-  this.vy = (Math.random()-0.5)*20;
-  this.accX = 0;
-  this.accY = 0;
-  this.friction = Math.random()*0.05 + 0.94;
+function Particle(x, y) {
+    this.x = Math.random() * ww;
+    this.y = Math.random() * wh;
+    this.dest = {
+        x: x,
+        y: y
+    };
+    this.r = Math.random() * 5 + 2;
+    this.vx = (Math.random() - 0.5) * 20;
+    this.vy = (Math.random() - 0.5) * 20;
+    this.accX = 0;
+    this.accY = 0;
+    this.friction = Math.random() * 0.05 + 0.94;
 
-  this.color = colors[Math.floor(Math.random()*6)];
+    this.color = colors[Math.floor(Math.random() * 6)];
 }
 
-Particle.prototype.render = function() {
+Particle.prototype.render = function () {
 
 
-  this.accX = (this.dest.x - this.x)/1000;
-  this.accY = (this.dest.y - this.y)/1000;
-  this.vx += this.accX;
-  this.vy += this.accY;
-  this.vx *= this.friction;
-  this.vy *= this.friction;
-
-  this.x += this.vx;
-  this.y +=  this.vy;
-
-  ctx.fillStyle = this.color;
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
-  ctx.fill();
-
-  var a = this.x - mouse.x;
-  var b = this.y - mouse.y;
-
-  var distance = Math.sqrt( a*a + b*b );
-  if(distance<(radius*70)){
-    this.accX = (this.x - mouse.x)/100;
-    this.accY = (this.y - mouse.y)/100;
+    this.accX = (this.dest.x - this.x) / 1000;
+    this.accY = (this.dest.y - this.y) / 1000;
     this.vx += this.accX;
     this.vy += this.accY;
-  }
+    this.vx *= this.friction;
+    this.vy *= this.friction;
 
-}
+    this.x += this.vx;
+    this.y += this.vy;
 
-function onMouseMove(e){
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-}
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+    ctx.fill();
 
-function onTouchMove(e){
-  if(e.touches.length > 0 ){
-    mouse.x = e.touches[0].clientX;
-    mouse.y = e.touches[0].clientY;
-  }
-}
+    var a = this.x - mouse.x;
+    var b = this.y - mouse.y;
 
-function onTouchEnd(e){
-mouse.x = -9999;
-mouse.y = -9999;
-}
-
-function initScene(){
-  ww = canvas.width = window.innerWidth;
-  wh = canvas.height = window.innerHeight;
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  ctx.font = "bold "+(ww/6)+"px Grape Nut";
-  ctx.textAlign = "center";
-  ctx.fillText(copy.value, ww/2, wh/2);
-
-  var data  = ctx.getImageData(0, 0, ww, wh).data;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.globalCompositeOperation = "screen";
-
-  particles = [];
-  for(var i=0;i<ww;i+=Math.round(ww/250)){
-    for(var j=0;j<wh;j+=Math.round(ww/250)){
-      if(data[ ((i + j*ww)*4) + 3] > 150){
-        particles.push(new Particle(i,j));
-      }
+    var distance = Math.sqrt(a * a + b * b);
+    if (distance < (radius * 70)) {
+        this.accX = (this.x - mouse.x) / 100;
+        this.accY = (this.y - mouse.y) / 100;
+        this.vx += this.accX;
+        this.vy += this.accY;
     }
-  }
-  amount = particles.length;
+
+}
+
+function onMouseMove(e) {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+}
+
+function onTouchMove(e) {
+    if (e.touches.length > 0) {
+        mouse.x = e.touches[0].clientX;
+        mouse.y = e.touches[0].clientY;
+    }
+}
+
+function onTouchEnd(e) {
+    mouse.x = -9999;
+    mouse.y = -9999;
+}
+
+function initScene() {
+    ww = canvas.width = window.innerWidth;
+    wh = canvas.height = window.innerHeight;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.font = "bold " + (ww / 6) + "px Grape Nut";
+    ctx.textAlign = "center";
+    ctx.fillText(copy.value, ww / 2, wh / 2);
+
+    var data = ctx.getImageData(0, 0, ww, wh).data;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.globalCompositeOperation = "screen";
+
+    particles = [];
+    for (var i = 0; i < ww; i += Math.round(ww / 250)) {
+        for (var j = 0; j < wh; j += Math.round(ww / 250)) {
+            if (data[((i + j * ww) * 4) + 3] > 150) {
+                particles.push(new Particle(i, j));
+            }
+        }
+    }
+    amount = particles.length;
 
 }
 
 
 function render(a) {
-  requestAnimationFrame(render);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < amount; i++) {
-    particles[i].render();
-  }
+    requestAnimationFrame(render);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < amount; i++) {
+        particles[i].render();
+    }
 };
 
 window.addEventListener("resize", initScene);
