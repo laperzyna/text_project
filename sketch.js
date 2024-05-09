ad3a9497a1a2663662a2e430f2f2d2aff62c6b81
laperzyna <90733086+laperzyna@users.noreply.github.com>
2024-05-08 17:05:52 -0700

    added new page

--- sketch.js
+++ sketch.js
@@ -1,5 +1,11 @@
+let customFont;
+let callToActionFont;
+let currentBox = 0;
+let showPrompt = true;
+
 function preload() {
   customFont = loadFont('./Inspiration-Regular.ttf');
+  callToActionFont = loadFont('./GrapeNuts-Regular.ttf');
 }
 
 function setup() {
@@ -13,51 +19,65 @@ function setup() {
 function draw() {
   background(0);
   translate(width / 2, height / 2);
-  frameRate(35); 
   let totalBoxes = 21;
-  let boxesToShow = floor(frameCount / 10);
-  let delayFrames = 30;
 
-  for (let i = 0; i < totalBoxes - 1; i++) {
-    if (i < boxesToShow - 1) {
-      let angle = i * 139.5;
-      let r = sqrt(i) * 75;
-      let x = r * cos(angle);
-      let y = r * sin(angle);
-      
+  if (showPrompt) {
+    textFont(callToActionFont);
+    fill(255);
+    textSize(32);
+    text("Press SPACE", 0, -50);
+    textFont(customFont);
+  } else {
+    for (let i = 0; i < totalBoxes - 1; i++) {
+      if (i < currentBox) {
+        let angle = i * 139.5;
+        let r = sqrt(i) * 75;
+        let x = r * cos(angle);
+        let y = r * sin(angle);
+
+        push();
+        translate(x, y);
+        rotate(angle);
+        stroke('#AF251E');
+        strokeWeight(2);
+        fill(0);
+        rect(0, 0, 65, 65);
+        fill(255);
+        stroke('white');
+        textSize(45);
+        text(i + 1, 0, -10);
+        pop();
+      }
+    }
+
+    // Draw the 21st box in the center
+    if (currentBox >= totalBoxes) {
+      let hover = abs(mouseX - width / 2) < 65 && abs(mouseY - height / 2) < 65;
       push();
-      translate(x, y);
-      rotate(angle);
       stroke('#AF251E');
       strokeWeight(2);
       fill(0);
-      rect(0, 0, 65, 65);
+      rect(0, 0, 130, 130);
       fill(255);
       stroke('white');
-      textSize(45);
-      text(i + 1, 0, -10);
+      textSize(65);
+      text(21, 0, -10); // 21st box
+      stroke(hover ? '0' : '#FF6347');
       pop();
+
+      if (hover && mouseIsPressed) {
+        window.open("https://laperzyna.github.io/text_project/writeOut.html");
+      }
     }
   }
+}
 
-  // Draw the 21st box in the center
-  if (boxesToShow >= totalBoxes + delayFrames / 10) {
-    let hover = abs(mouseX - width / 2) < 65 && abs(mouseY - height / 2) < 65;
-    push();
-    stroke('#AF251E');
-    strokeWeight(2);
-    stroke(hover ? '0' : '#FF6347');
-    fill(0);
-    rect(0, 0, 130, 130);
-    fill(255);
-    stroke('white');
-    textSize(65);
-    text(21, 0, -10); // 21st box
-    pop();
-
-    // If clicked, you can redirect or perform another action
-    if (hover && mouseIsPressed) {
-      window.open("https://laperzyna.github.io/text_project/writeOut.html");
+function keyPressed() {
+  if (keyCode === 32) {  // If the space bar is pressed
+    if (showPrompt) {
+      showPrompt = false; // Stop showing the prompt
     }
+    currentBox++;
+    currentBox = min(currentBox, 21);  // Ensure it doesn't exceed 21
   }
 }
